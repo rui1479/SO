26d4b107b48312ed472a5bf40b15d4a83b1be768
Rui Ribeiro <ruiribeiro2003.rr@gmail.com>
2023-04-26 21:05:33 +0100

    tempo a dar

--- src/monitor.c
+++ src/monitor.c
@@ -1,154 +1,45 @@
-// Compile with: gcc monitor.c -o monitor -lpthread
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
-#include <string.h>
+#include <sys/types.h>
 #include <sys/stat.h>
-#include <pthread.h>
-#include <time.h>
+#include <sys/wait.h>
+#include <sys/time.h>
 #include <errno.h>
 
-
-#define PIPE_NAME "/tmp/tracer_pipe"
-#define MAX_BUFFER_SIZE 256
-#define MAX_COMMAND_SIZE 128
-
-typedef struct ProcessInfo {
-    int pid;
-    char cmd[MAX_COMMAND_SIZE];
-    long long start_time;
-    long long end_time;
-    struct ProcessInfo *next;
-} ProcessInfo;
-
-ProcessInfo *head = NULL;
-pthread_mutex_t lock;
-
-void add_process(ProcessInfo **head, int pid, const char *cmd, long long start_time) {
-    ProcessInfo *new_process = (ProcessInfo *)malloc(sizeof(ProcessInfo));
-    new_process->pid = pid;
-    strncpy(new_process->cmd, cmd, MAX_COMMAND_SIZE - 1);
-    new_process->cmd[MAX_COMMAND_SIZE - 1] = '\0';
-    new_process->start_time = start_time;
-    new_process->end_time = 0;
-    new_process->next = *head;
-    *head = new_process;
-}
-
-void update_process_end_time(ProcessInfo *head, int pid, long long end_time) {
-    ProcessInfo *current = head;
-    while (current != NULL) {
-        if (current->pid == pid) {
-            current->end_time = end_time;
-            return;
-        }
-        current = current->next;
-    }
-}
-
-void print_running_processes(ProcessInfo *head, int client_fd) {
-    ProcessInfo *current = head;
-    char buffer[MAX_BUFFER_SIZE];
-    while (current != NULL) {
-        if (current->end_time == 0) {
-            long long duration = (long long)(time(NULL) - current->start_time) * 1000;
-            snprintf(buffer, MAX_BUFFER_SIZE, "PID: %d, CMD: %s, EXECUTION_TIME: %lld ms\n", current->pid, current->cmd, duration);
-            write(client_fd, buffer, strlen(buffer));
-        }
-        current = current->next;
-    }
-}
-
-void *handle_client(void *arg) {
-    int client_fd = *((int *)arg);
-    free(arg);
-
-    char buffer[MAX_BUFFER_SIZE];
-    int num_read = read(client_fd, buffer, MAX_BUFFER_SIZE - 1);
-    if (num_read > 0) {
-        buffer[num_read] = '\0';
-
-        if (strcmp(buffer, "status") == 0) {
-            pthread_mutex_lock(&lock);
-            print_running_processes(head, client_fd);
-            pthread_mutex_unlock(&lock);
-        } else {
-            int pid;
-            char cmd[MAX_COMMAND_SIZE];
-            long long start_time;
-            sscanf(buffer, "%d %[^|] %lld", &pid, cmd, &start_time);
-
-            pthread_mutex_lock(&lock);
-            add_process(&head, pid, cmd, start_time);
-            pthread_mutex_unlock(&lock);
-        }
-    }
-
-    close(client_fd);
-    return NULL;
-}
-
 int main() {
-    mkfifo(PIPE_NAME, 0666);
+    printf("Servidor iniciado\n");
 
-    int pipe_fd = open(PIPE_NAME, O_RDONLY | O_NONBLOCK);
+    mkfifo("monitor_fifo", 0666);
+    int pipe_fd = open("monitor_fifo", O_RDONLY | O_NONBLOCK);
     if (pipe_fd == -1) {
-        perror("Erro ao abrir pipe");
-        return 1;
+        perror("Falha ao abrir pipe");
+        exit(EXIT_FAILURE);
     }
-
-    pthread_mutex_init(&lock, NULL);
 
     while (1) {
-        int client_fd = open(PIPE_NAME,O_WRONLY | O_NONBLOCK);
-    if (client_fd != -1) {
-        pthread_t thread;
-        int *arg = malloc(sizeof(int));
-        *arg = client_fd;
-        pthread_create(&thread, NULL, handle_client, arg);
-        pthread_detach(thread);
-    } else {
-        if (errno != EAGAIN) {
-            perror("Erro ao abrir pipe do cliente");
-        break;
+        pid_t pid;
+        char program_name[256];
+        struct timeval timestamp;
+
+        ssize_t pid_read = read(pipe_fd, &pid, sizeof(pid_t));
+        ssize_t program_name_read = read(pipe_fd, program_name, sizeof(program_name));
+        ssize_t timestamp_read = read(pipe_fd, &timestamp, sizeof(struct timeval));
+
+        if (pid_read > 0 && program_name_read > 0 && timestamp_read > 0) {
+            printf("Programa \"%s\" com PID %d iniciou em %ld.%06ld\n", program_name, pid, (long)timestamp.tv_sec, (long)timestamp.tv_usec);
+        } else if (pid_read == -1 || program_name_read == -1 || timestamp_read == -1) {
+            if (errno != EAGAIN) {
+                perror("Erro ao ler dados do pipe");
+                exit(EXIT_FAILURE);
+            }
         }
-    }
-        char buffer[MAX_BUFFER_SIZE];
-    int num_read = read(pipe_fd, buffer, MAX_BUFFER_SIZE - 1);
-    if (num_read > 0) {
-        buffer[num_read] = '\0';
 
-        int pid;
-        char cmd[MAX_COMMAND_SIZE];
-        long long start_time;
-        sscanf(buffer, "%d %[^|] %lld", &pid, cmd, &start_time);
-
-        pthread_mutex_lock(&lock);
-        add_process(&head, pid, cmd, start_time);
-        pthread_mutex_unlock(&lock);
-    } else {
-        if (errno != EAGAIN) {
-            perror("Erro ao ler do pipe");
-            break;
-        }
-    }
-
-    sleep(1);
+        usleep(100000);  // Dorme por 100 ms para evitar consumo excessivo de CPU
     }
 
     close(pipe_fd);
-    unlink(PIPE_NAME);
-
-    pthread_mutex_destroy(&lock);
-
-    ProcessInfo *current = head;
-    while (current != NULL) {
-        printf("PID: %d, CMD: %s, START_TIME: %lld, END_TIME: %lld\n", current->pid, current->cmd, current->start_time, current->end_time);
-        ProcessInfo *temp = current;
-        current = current->next;
-        free(temp);
-    }
-
-return 0;
+    unlink("pipe_monitor");
+    return 0;
 }
--- src/tracer.c
+++ src/tracer.c
@@ -1,142 +1,84 @@
-// Compile with: gcc tracer.c -o tracer
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
-#include <sys/wait.h>
-#include <sys/types.h>
-#include <sys/time.h>
 #include <fcntl.h>
-#include <errno.h>
-
-#define PIPE_NAME "/tmp/tracer_pipe"
-#define MAX_BUFFER_SIZE 256
-
-long long current_time_ms() {
-    struct timeval tv;
-    gettimeofday(&tv, NULL);
-    return (long long)tv.tv_sec * 1000 + tv.tv_usec / 1000;
-}
-
-void execute_program(const char *cmd_line) {
-    char *cmd_args[MAX_BUFFER_SIZE];
-    char *token;
-    int i = 0;
-
-    token = strtok(cmd_line, " ");
-    while (token != NULL && i < MAX_BUFFER_SIZE - 1) {
-        cmd_args[i++] = token;
-        token = strtok(NULL, " ");
-    }
-    cmd_args[i] = NULL;
-
-    execute_program_with_args(cmd_args[0], cmd_args);
-}
-
-void execute_program_with_args(const char *cmd, char *const argv[]) {
-    int pid = fork();
-
-    if (pid < 0) {
-        perror("Erro ao criar processo");
-        exit(1);
-    }
-
-    if (pid == 0) {
-        if (execvp(cmd, argv) < 0) {
-            perror("Erro ao executar programa");
-            exit(1);
-        }
-    } else {
-        printf("Executando programa com PID: %d\n", pid);
-
-        long long start_time = current_time_ms();
-        int pipe_fd = open(PIPE_NAME, O_WRONLY);
-        if (pipe_fd < 0) {
-            perror("Erro ao abrir pipe");
-            exit(1);
-        }
-
-        char buffer[MAX_BUFFER_SIZE];
-        snprintf(buffer, MAX_BUFFER_SIZE, "%d %s %lld", pid, cmd, start_time);
-        write(pipe_fd, buffer, strlen(buffer));
-        close(pipe_fd);
-
-        int status;
-        waitpid(pid, &status, 0);
-        long long end_time = current_time_ms();
-
-        pipe_fd = open(PIPE_NAME, O_WRONLY);
-        if (pipe_fd < 0) {
-            perror("Erro ao abrir pipe");
-            exit(1);
-        }
-
-        snprintf(buffer, MAX_BUFFER_SIZE, "%d %lld", pid, end_time);
-        write(pipe_fd, buffer, strlen(buffer));
-        close(pipe_fd);
-
-        printf("Programa finalizado. Tempo de execução: %lld ms\n", end_time - start_time);
-    }
-}
-
-void query_running_programs() {
-    int pipe_fd = open(PIPE_NAME, O_WRONLY);
-    if (pipe_fd < 0) {
-        perror("Erro ao abrir pipe");
-        exit(1);
-    }
-
-    write(pipe_fd, "status", strlen("status"));
-    close(pipe_fd);
-
-    pipe_fd = open(PIPE_NAME, O_RDONLY);
-    if (pipe_fd < 0) {
-        perror("Erro ao abrir pipe");
-        exit(1);
-    }
-
-    char buffer[MAX_BUFFER_SIZE];
-    while (1) {
-        ssize_t num_read = read(pipe_fd, buffer, MAX_BUFFER_SIZE - 1);
-        if (num_read > 0) {
-            buffer[num_read] = '\0';
-            printf("%s", buffer);
-        } else if (num_read == 0) {
-            break;
-        } else {
-            if (errno != EAGAIN) {
-                perror("Erro ao ler do pipe");
-                break;
-            }
-        }
-    }
-
-    close(pipe_fd);
-}
-
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <sys/time.h>
 
 int main(int argc, char *argv[]) {
-    if (argc < 2) {
-        fprintf(stderr, "Uso: %s <execute|status> [-u] [cmd] [args]\n", argv[0]);
-        exit(1);
+
+    mkfifo("tracer_fifo", 0666);
+    int pipe_fd = open("tracer_fifo", O_RDONLY | O_NONBLOCK);
+    if (pipe_fd == -1) {
+        perror("Falha ao abrir pipe");
+        exit(EXIT_FAILURE);
+    }
+    if (argc < 3) {
+        printf("Uso: %s execute -u <programa> [argumentos]\n", argv[0]);
+        exit(EXIT_FAILURE);
     }
 
-    if (strcmp(argv[1], "execute") == 0) {
-        if (argc < 4) {
-            fprintf(stderr, "Uso: %s execute -u [cmd] [args]\n", argv[0]);
-            exit(1);
+    if (strcmp(argv[1], "execute") == 0 && strcmp(argv[2], "-u") == 0) {
+        int pipe_fd = open("pipe_monitor", O_WRONLY);
+        if (pipe_fd == -1) {
+            perror("Falha ao abrir pipe");
+            exit(EXIT_FAILURE);
         }
-        if (strcmp(argv[2], "-u") == 0) {
-            execute_program(argv[3]);
+
+        pid_t pid = fork();
+
+        if (pid == -1) {
+            perror("Falha ao criar processo filho");
+            exit(EXIT_FAILURE);
+        }
+
+        if (pid == 0) {
+            close(pipe_fd);
+            execvp(argv[3], &argv[3]);
+            perror("Falha ao executar o programa");
+            exit(EXIT_FAILURE);
         } else {
-            fprintf(stderr, "Opção inválida. Use -u para executar um programa.\n");
-            exit(1);
+            struct timeval start_time, end_time;
+            gettimeofday(&start_time, NULL);
+
+            printf("PID do programa: %d\n", pid);
+            write(pipe_fd, &pid, sizeof(pid_t));
+            write(pipe_fd, argv[3], strlen(argv[3]) + 1);
+            write(pipe_fd, &start_time, sizeof(struct timeval));
+
+            int status;
+            waitpid(pid, &status, 0);
+            gettimeofday(&end_time, NULL);
+
+            long elapsed_time = (end_time.tv_sec - start_time.tv_sec) * 1000000 + (end_time.tv_usec - start_time.tv_usec);
+            int elapsed_time_ms = elapsed_time / 10; // Converter para milissegundos
+            printf("Programa executado com sucesso\n");
+            printf("Tempo de execução: %i milissegundos\n", elapsed_time_ms);
+
+            char filename[256];
+            snprintf(filename, sizeof(filename), "PIDS-folder/%d.txt", pid);
+            FILE *file = fopen(filename, "w");
+            if (file == NULL) {
+                perror("Erro ao abrir o arquivo");
+                exit(EXIT_FAILURE);
+            }
+            fprintf(file, "PID do programa: %d\n", pid);
+            fprintf(file, "Comando usado: %s", argv[3]);
+            for (int i = 4; i < argc; i++) {
+                fprintf(file, " %s", argv[i]);
+            }
+            fprintf(file, "\nTempo de execução: %i milissegundos\n", elapsed_time_ms);
+            fclose(file);
+
+
+            close(pipe_fd);
         }
-    } else if (strcmp(argv[1], "status") == 0) {
-        query_running_programs();
     } else {
-        fprintf(stderr, "Comando inválido. Use 'execute' para executar um programa ou 'status' para consultar o status.\n");
-        exit(1);
+        printf("Opção inválida\n");
+        exit(EXIT_FAILURE);
     }
 
     return 0;
