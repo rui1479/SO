47c3691751cb3efec05631f119f0dafd808c442f
Rui Ribeiro <ruiribeiro2003.rr@gmail.com>
2023-04-27 12:13:02 +0100

    ./monitor com caminho

--- src/monitor.c
+++ src/monitor.c
@@ -7,8 +7,145 @@
 #include <sys/wait.h>
 #include <sys/time.h>
 #include <errno.h>
+#include <string.h>
 
-int main() {
+#define PATH_MAX 4096
+
+char* pids_folder_path; // Variável global para armazenar o caminho da pasta
+
+
+void handle_stats_time(char *pid_list)
+{
+    char *token = strtok(pid_list, " ");
+    int total_time = 0;
+
+    while (token)
+    {
+        char filename[256];
+        snprintf(filename, sizeof(filename), "/home/username/Desktop/SO/src/PIDS-folder/%s.txt", token);
+
+        FILE *file = fopen(filename, "r");
+        if (file == NULL)
+        {
+            perror("Erro ao abrir o arquivo");
+            exit(EXIT_FAILURE);
+        }
+
+        int exec_time;
+        // Pular as duas primeiras linhas
+        fseek(file, 0, SEEK_SET);
+        fscanf(file, "%*[^\n]\n%*[^\n]\n");
+        // Ler a terceira linha para obter o tempo de execução
+        fscanf(file, "Tempo de execução: %i milissegundos\n", &exec_time);
+
+        total_time += exec_time;
+
+        fclose(file);
+
+        token = strtok(NULL, " ");
+    }
+
+    printf("Tempo total de execução: %i milissegundos\n", total_time);
+}
+
+void handle_stats_command(char *pid_list, char *command)
+{
+    char *token = strtok(pid_list, ",");
+    int count = 0;
+
+    while (token)
+    {
+        char filename[256];
+        snprintf(filename, sizeof(filename), "/home/username/Desktop/SO/src/PIDS-folder/%s.txt", token);
+
+        FILE *file = fopen(filename, "r");
+        if (file == NULL)
+        {
+            perror("Erro ao abrir o arquivo");
+            exit(EXIT_FAILURE);
+        }
+
+        char exec_command[256];
+        // Pular a primeira linha
+        fseek(file, 0, SEEK_SET);
+        fscanf(file, "%*[^\n]\n");
+        // Ler a segunda linha para obter o comando usado
+        fscanf(file, "Comando usado: %s", exec_command);
+
+        if (strcmp(exec_command, command) == 0)
+        {
+            count++;
+        }
+
+        fclose(file);
+
+        token = strtok(NULL, ",");
+    }
+
+    printf("Número de vezes que o comando %s foi executado: %i\n", command, count);
+}
+
+void handle_stats_uniq(char *pid_list)
+{
+    char *token = strtok(pid_list, ",");
+    char unique_commands[256][256];
+    int unique_count = 0;
+
+    while (token)
+    {
+        char filename[256];
+        snprintf(filename, sizeof(filename), "/home/username/Desktop/SO/src/PIDS-folder/%s.txt", token);
+
+        FILE *file = fopen(filename, "r");
+        if (file == NULL)
+        {
+            perror("Erro ao abrir o arquivo");
+            exit(EXIT_FAILURE);
+        }
+
+        char exec_command[256];
+        // Pular a primeira linha
+        fseek(file, 0, SEEK_SET);
+        fscanf(file, "%*[^\n]\n");
+        // Ler a segunda linha para obter o comando usado
+        fscanf(file, "Comando usado: %s", exec_command);
+
+        int is_unique = 1;
+        for (int i = 0; i < unique_count; i++)
+        {
+            if (strcmp(unique_commands[i], exec_command) == 0)
+            {
+                is_unique = 0;
+                break;
+            }
+        }
+
+        if (is_unique)
+        {
+            strncpy(unique_commands[unique_count], exec_command, sizeof(exec_command));
+            unique_count++;
+        }
+
+        fclose(file);
+        token = strtok(NULL, ",");
+    }
+
+    printf("Número de comandos únicos executados: %i\n", unique_count);
+    for (int i = 0; i < unique_count; i++)
+    {
+        printf("Comando único: %s\n", unique_commands[i]);
+    }
+}
+
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        printf("Uso: %s <caminho_da_pasta>\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    pids_folder_path = argv[1]; // Armazenar o caminho da pasta
+
     printf("Servidor iniciado\n");
 
     mkfifo("monitor_fifo", 0666);
